e898ad7693b38abb169bd2b613817213dbe2cd1f
Dennis Lundberg <dennis.p.lundberg@gmail.com>
2018-09-11 16:51:54 +0200

    Misc edits

--- init.el
+++ init.el
@@ -30,6 +30,7 @@
 (load-library "theming.el")
 (load-library "modeline.el")
 
+;; Start the emacs server
 (require 'server)
 (unless (server-running-p) (server-start))
 
@@ -38,9 +39,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(markdown-command "pandoc")
  '(package-selected-packages
    (quote
-    (rust-mode ace-window doom-themes doom-theme eldoc-eval golden-ratio powerline-evil evil-powerline fill-column-indicator git-gutter evil-magit magit rainbow-delimiters smartparens evil-multiedit all-the-icons ess auto-complete helm-config window-numbering window-number rainbow-mode markdown-mode markdowm-mode f dash avy helm-swoop helm-projectile helm counsel-projectile projectile powerline evil which-key swiper general use-package))))
+    (lua-mode multiple-cursors flycheck jedi py-autopep8 autopep8 elpy neotree rust-mode ace-window doom-themes doom-theme eldoc-eval golden-ratio powerline-evil evil-powerline fill-column-indicator git-gutter evil-magit magit rainbow-delimiters smartparens evil-multiedit all-the-icons ess auto-complete helm-config window-numbering window-number rainbow-mode markdown-mode markdowm-mode f dash avy helm-swoop helm-projectile helm counsel-projectile projectile powerline evil which-key swiper general use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
